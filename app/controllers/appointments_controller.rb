ef3ccc11002fcf579e502df00afb0f14fdfdbe99
Nicholas Njeru <nicholasnjeru917@gmail.com>
2023-02-12 17:43:22 +0300

    some updates

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,6 @@
 class AppointmentsController < ApplicationController
-    
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
         def index 
         appointments = Appointment.all
         render json: appointments, status: :ok
@@ -31,5 +32,13 @@ class AppointmentsController < ApplicationController
 
         def appointment_params
         params.permit(:patient_id, :doctor_id, :appointment_date, :appointment_time, :appointment_status, :appointment_notes)
+        end
+
+        def record_not_found
+         render json: {error: "Appointment not found"}, status: :not_found
+        end
+
+        def record_invalid invalid
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
         end
 end
